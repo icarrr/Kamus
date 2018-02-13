2fffc4182737effd5debd3c68861591cd8b22f02
icarrr <faizarsangster@gmail.com>
2018-02-13 22:52:41 +0800

    Test git untuk python

---
+++ kamus.py
@@ -0,0 +1,9 @@
+print "="*31,"Kamus Bugis-Jawa","="*31
+
+kalimat1 = [{'Bahasa Indonesia': 'Nama kamu siapa?', 'Bugis': 'Iga asennu?', 'Jawa': 'Janengmu sopo?'}]
+kata1 = [{'Bahasa Indonesia': 'Siapa', 'Bugis': 'Iga', 'Jawa': 'Sopo'}]
+for k in kalimat1:
+    print k
+
+
+
